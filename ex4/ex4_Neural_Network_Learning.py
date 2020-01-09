9f13e8a2fc95eac248dfded61db5b222c6e73e11
hanfeiyu <hanfeiyu@uw.edu>
2020-01-09 14:30:29 -0800

    add ex4

---
+++ ex4/ex4_Neural_Network_Learning.py
@@ -0,0 +1,13 @@
+#
+# Exercise 4 of ML Andrew NG, neural network learning
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io 
+import scipy.optimize as opt
+import random as rd
+import matplotlib.cm as cm
+
+
+# Import data from ex4data1.mat
